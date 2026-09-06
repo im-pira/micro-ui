85eae7771fa351d6bd8025493f9cb5526b0e54f5
im-pira <heyluna1811@gmail.com>
2026-09-06 19:32:51 +0530

    code clean

--- src/experiments/shapes/Shapes.tsx
+++ src/experiments/shapes/Shapes.tsx
@@ -3,51 +3,35 @@ export default function Shapes() {
     <div className="min-h-screen bg-neutral-950 p-6">
       <div className="grid min-h-[calc(100vh-3rem)] grid-cols-5 grid-rows-3 place-items-center">
 
-        {/* Shape 01 - Drop Shadow */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 02 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 03 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 04 */}
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        {/* Shape 05 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
 
-        {/* Shape 06 */}
+
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 07 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 08 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 09 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 10 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
 
-        {/* Shape 11 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 12 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 13 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 14 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
-        {/* Shape 15 */}
         <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
 
       </div>
