8baa0dc81d8a0c417933e89dc9a937ff9b7cef16
im-pira <heyluna1811@gmail.com>
2026-09-06 23:07:18 +0530

    Texture overlay

--- src/experiments/shapes/Shapes.tsx
+++ src/experiments/shapes/Shapes.tsx
@@ -6,7 +6,7 @@ export default function Shapes() {
         {/* Raised neumorphism */}
         <div className="h-40 w-40 rounded-2xl bg-[#e8e8e8] shadow-[-10px_-10px_20px_#fff,10px_10px_20px_#c5c5c5]" />
 
-        
+
         {/* Extruded neumorphism */}
         <div className="h-40 w-40 rounded-[28px] bg-gradient-to-tr from-white via-[#eeeeee] to-[#c9c9c9] shadow-[-10px_14px_12px_rgba(0,0,0,.28),_8px_12px_10px_rgba(0,0,0,.18),_inset_2px_2px_3px_white]" />
 
@@ -16,7 +16,10 @@ export default function Shapes() {
         {/* Beveled / embossed neumorphism */}
         <div className="h-40 w-40 rounded-2xl bg-[#f1f1f1] shadow-[inset_5px_5px_8px_#fff,inset_-5px_-5px_8px_#cfcfcf,7px_9px_12px_rgba(0,0,0,.18)] border border-white/70" />
 
-        <div className="h-40 w-40 rounded-2xl bg-white" />
+
+        {/* Texture overlay / paper grain card */}
+        <div className="h-40 w-40 rounded-2xl bg-[#f5f5f2] shadow-[6px_8px_14px_rgba(0,0,0,.14)] bg-[repeating-radial-gradient(circle_at_0_0,rgba(0,0,0,.035)_0_1px,transparent_1px_3px)]" />
+        
         <div className="h-40 w-40 rounded-2xl bg-white" />
         <div className="h-40 w-40 rounded-2xl bg-white" />
         <div className="h-40 w-40 rounded-2xl bg-white" />
