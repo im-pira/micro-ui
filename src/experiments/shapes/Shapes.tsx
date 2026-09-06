d4f814b3cb266be98a477de69a00e29358ecb5bc
im-pira <heyluna1811@gmail.com>
2026-09-06 23:05:45 +0530

    raised neumorphism

--- src/experiments/shapes/Shapes.tsx
+++ src/experiments/shapes/Shapes.tsx
@@ -1,38 +1,25 @@
 export default function Shapes() {
   return (
-    <div className="min-h-screen bg-neutral-950 p-6">
+    <div className="min-h-screen bg-[#e8e8e8] p-6">
       <div className="grid min-h-[calc(100vh-3rem)] grid-cols-5 grid-rows-3 place-items-center">
 
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
+        {/* Raised neumorphism */}
+        <div className="h-40 w-40 rounded-2xl bg-[#e8e8e8] shadow-[-10px_-10px_20px_#fff,10px_10px_20px_#c5c5c5]" />
 
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
 
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
-
-        <div className="h-40 w-40 rounded-2xl bg-neutral-800" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
+        <div className="h-40 w-40 rounded-2xl bg-white" />
 
       </div>
     </div>
