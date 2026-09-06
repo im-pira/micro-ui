bcd7411240bbcec2b7024d2bc016fb1c2c79a847
im-pira <heyluna1811@gmail.com>
2026-09-06 23:06:09 +0530

    extruded neumorphism

--- src/experiments/shapes/Shapes.tsx
+++ src/experiments/shapes/Shapes.tsx
@@ -6,7 +6,10 @@ export default function Shapes() {
         {/* Raised neumorphism */}
         <div className="h-40 w-40 rounded-2xl bg-[#e8e8e8] shadow-[-10px_-10px_20px_#fff,10px_10px_20px_#c5c5c5]" />
 
-        <div className="h-40 w-40 rounded-2xl bg-white" />
+        
+        {/* Extruded neumorphism */}
+        <div className="h-40 w-40 rounded-[28px] bg-gradient-to-tr from-white via-[#eeeeee] to-[#c9c9c9] shadow-[-10px_14px_12px_rgba(0,0,0,.28),_8px_12px_10px_rgba(0,0,0,.18),_inset_2px_2px_3px_white]" />
+
         <div className="h-40 w-40 rounded-2xl bg-white" />
         <div className="h-40 w-40 rounded-2xl bg-white" />
         <div className="h-40 w-40 rounded-2xl bg-white" />
